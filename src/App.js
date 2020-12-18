48ab174bdb44aa99e465eb40c1adec61b3f29271
Dissmind <dissmind@gmail.com>
2020-12-18 21:53:17 +0200

    storybook init

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 // import About from '.src/about/about'
 
 import About from "./components/About/about";
+import React from 'react'
+import { Feedback } from './components/Feedback'
 
 function App() {
   return (
     <div className="App">
       <About />
+      <Feedback />
     </div>
   );
 }
---
+++ src/components/Feedback/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export const Feedback = () => (
+    <div>
+        Feedback
+    </div>
+)
