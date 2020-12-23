af14dfb7949423f703676d15ffe6b794a5d94ceb
Dissmind <dissmind@gmail.com>
2020-12-23 07:48:32 +0200

    [ADD]: slider from UI

--- src/UI/Slider/index.jsx
+++ src/UI/Slider/index.jsx
@@ -1,21 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 
 const StlSlider = styled.div`
-    width: 60%;
-    heigh: 70vh;
-    border: solid black 2px;
+    width: 90%;
+    height: 70vh;
+    display: flex;
+    width: 100%;
+    overflow-x: hidden;
+    justify-content: center;
 `
 
-const arrowButton = '<'
+
+export const Slider = ({cards}) => {
+
+    const [cardId, setCardId] = useState([0, 1])
+    const [status, setStatus] = useState(null)
+
+    const prevHandler = () => {
+        if ((cardId[1] - 1) < 0) return
+
+        setStatus('left')
+        setTimeout(() => {
+            setStatus(null)
+            setCardId([(cardId[0] - 1), (cardId[1] - 1)])
+        }, 400)
+    }
+    const nextHandler = () => {
+        if ((cardId[1] + 1) > cards.length) return
+
+        setStatus('right')
+        setTimeout(() => {
+            setStatus(null)
+            setCardId([(cardId[0] + 1), (cardId[1] + 1)])
+        }, 400)
+    }
 
 
+    const toggleMove = () => {
+        const StlCardMoveLeft = {
+            transform: 'translateX(-100%)',
+            transition: '1s ease',
+        }
+
+        const StlCardMoveRight = {
+            transform: 'translateX(100%)',
+            transition: '1s ease',
+        }
+
+        switch(status) {
+            case 'left':
+                return StlCardMoveLeft
+            case 'right':
+                return StlCardMoveRight
+            default:
+                return null
+        }
+    }
 
 
+    return (
+        <>
+            <div style={toggleMove()}>
+                <StlSlider>
+                    {cards[cardId[0]]}
+                    {cards[cardId[1]]}
+                </StlSlider>
+            </div>
 
-const Slider = ({cards}) => (
-    <StlSlider>
-
-    </StlSlider>
-)
+            <button
+                onClick={prevHandler}
+            >Prev</button>
+            <button
+                onClick={nextHandler}
+            >Next</button>
+        </>
+    )
+}
---
+++ src/UI/Slider/index.stories.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+import { Slider } from './index.jsx'
+import { Card } from '../Card'
+
+
+export default {
+    title: 'UI/Slider',
+    component: Slider,
+}
+
+const Template = (args) => <Slider {...args} />
+
+
+export const Primary = Template.bind({})
+Primary.args = {
+    cards: [
+        <Card
+            cardTitle="Продажи"
+            cardType="VIP-формат"
+            list = {["Психология продаж", "Владение переговорами", "Создание вечных отношений\n" +
+            "с клиентом", "Ценности и боли клиентов"]}
+            timeStudy="12 месяцев"
+            price="14 999"
+
+            summaryHeight="577px"
+
+            borderWidth="450px"
+            borderHeight="560px"
+            borderColor="#E7EAEF"
+
+            backgroundWidth="455px"
+            backgroundHeight="560px"
+            backgroundColor="#952D31"
+
+            fontColor="#FFFFFF"
+        />,
+        <Card
+            cardTitle="Продажи"
+            cardType="Базовый формат"
+            list = {["Психология продаж", "Владение переговорами", "Создание вечных отношений\n" +
+            "с клиентом", "Ценности и боли клиентов"]}
+            timeStudy="12 месяцев"
+            price="9 999"
+
+            summaryHeight="577px"
+            cardMarginLeftRight="60px"
+
+            borderWidth="450px"
+            borderHeight="560px"
+            borderColor="#952D31"
+
+            backgroundWidth="455px"
+            backgroundHeight="560px"
+            backgroundColor="#E7EAEF"
+
+            fontColor="#952D31"
+        />,
+        <Card
+            cardTitle="Продажи"
+            cardType="Эконом-формат"
+            list = {["Психология продаж", "Владение переговорами", "Создание вечных отношений\n" +
+            "с клиентом", "Ценности и боли клиентов"]}
+            timeStudy="12 месяцев"
+            price="4 999"
+
+            summaryHeight="577px"
+
+            borderWidth="450px"
+            borderHeight="560px"
+            borderColor="#952D31"
+
+            backgroundWidth="455px"
+            backgroundHeight="560px"
+            backgroundColor="#FFFFFF"
+
+            fontColor="#952D31"
+        />
+    ]
+}
+
+
+// export const Second = Template.bind({})
+// Primary.args = {
+//
+// }
