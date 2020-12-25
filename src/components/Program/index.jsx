bd2e815905c2a5bffc88766545b81be9a4f95de9
Dissmind <dissmind@gmail.com>
2020-12-25 03:24:29 +0200

    minore change

--- src/components/Program/index.jsx
+++ src/components/Program/index.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import './style.css'
+
 import TitleBackground from "./TitleBackground/TitleBackground";
 import Title from "../../UI/Title";
 import {Card} from "../../UI/Card";
-import liWhiteImage from "../../attachments/img/li-white.png"
-import liRedImage from "../../attachments/img/li-red.png"
 import { useSelector, useDispatch } from 'react-redux'
 import {
     selectCards,
     fetchData,
 } from './index.reducer.js'
+
 
 export const Program = () => {
     const cards = useSelector(selectCards)
@@ -31,7 +31,8 @@ export const Program = () => {
                 />
             </TitleBackground>
             <div className="CardBlock">
-                {cards.map(i => (
+                {
+                    cards.map(i => (
                     <Card
                         cardTitle={i.cardTitle}
                         cardType={i.cardType}
@@ -39,17 +40,8 @@ export const Program = () => {
                         timeStudy={i.timeStudy}
                         price={i.price}
                         themeNumber={i.themeNumber}
-
-                        summaryHeight="577px"
-                        borderWidth="450px"
-                        borderHeight="560px"
-                        borderColor="#E7EAEF"
-                        backgroundWidth="455px"
-                        backgroundHeight="560px"
-                        backgroundColor="#952D31"
-                        fontColor="#FFFFFF"
                     />))
-                    }
+                }
             </div>
             <div className="ButtonMore">Узнать больше информации о курсах</div>
         </div>
