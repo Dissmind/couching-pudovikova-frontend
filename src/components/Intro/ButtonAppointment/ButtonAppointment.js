9f55b4dd3ed624c24c4b82b86ea92881e1fe1b0a
Dissmind <dissmind@gmail.com>
2020-12-25 04:31:19 +0200

    [ADD]: scroll

--- src/components/Intro/ButtonAppointment/ButtonAppointment.js
+++ src/components/Intro/ButtonAppointment/ButtonAppointment.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import './ButtonAppointment.css'
+import { animateScroll as scroll, scroller, Link } from 'react-scroll'
+import { LinkTo } from '@storybook/addon-links'
 
 function ButtonAppointment(props) {
+
+    const goToProgram = () => {
+        scroller.scrollTo('program-anchor', {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart'
+        })
+    }
+
     return (
-        <div className="ButtonAppointment">
-            <div>
-                {props.content}
+        // <Link to={"program-anchor"}>
+            <div
+                className="ButtonAppointment"
+                onClick={goToProgram}
+            >
+                <div>
+                    {props.content}
+                </div>
             </div>
-        </div>
+        // </Link>
     )
 }
 
--- src/components/Program/index.jsx
+++ src/components/Program/index.jsx
@@ -3,13 +3,13 @@ import './style.css'
 
 import TitleBackground from "./TitleBackground/TitleBackground";
 import Title from "../../UI/Title";
-import {Card} from "../../UI/Card";
+import {Card} from "./Card";
 import { useSelector, useDispatch } from 'react-redux'
 import {
     selectCards,
     fetchData,
 } from './index.reducer.js'
-
+import { Element, scroller } from 'react-scroll'
 
 export const Program = () => {
     const cards = useSelector(selectCards)
@@ -19,8 +19,17 @@ export const Program = () => {
         dispatch(fetchData())
     }, [])
 
+    const goToRecord = () => {
+        scroller.scrollTo('record-anchor', {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart'
+        })
+    }
+
     return (
-        <div className="Program">
+        <div className="Program program-anchor">
+            <Element name="program-anchor"/>
             <TitleBackground>
                 <Title
                     content="Темы обучения"
@@ -43,7 +52,10 @@ export const Program = () => {
                     />))
                 }
             </div>
-            <div className="ButtonMore">Узнать больше информации о курсах</div>
+            <div
+                onClick={goToRecord}
+                className="ButtonMore"
+            >Узнать больше информации о курсах</div>
         </div>
     )
 }
