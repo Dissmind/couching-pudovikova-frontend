61d21e00470c5a919bd73c6ec09b24aef2a85c63
SIDExSIDE <anoshkinzz@mail.ru>
2020-12-25 04:36:45 +0200

    ADD: Feedback

---
+++ src/components/Feedback/Popup/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css'
+
+export const PopupFeedback = () => (
+    <div className="PopupFeedback">
+        <div className="Info">
+            <div className="FileForm"></div>
+            <input type="file" className="ButtonAddPhoto"></input>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+)
