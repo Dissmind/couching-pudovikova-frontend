84bc53a873ad9d7acd7ec1b6d54b6f368ac91dec
SIDExSIDE <anoshkinzz@mail.ru>
2020-12-26 04:51:58 +0200

    Finished PopupFeedback

--- src/components/Feedback/Popup/index.js
+++ src/components/Feedback/Popup/index.js
@@ -4,19 +4,18 @@ import logoMail from '../../../attachments/img/logoMailWhite.png'
 
 export const PopupFeedback = ({close}) => (
     <>
-        <div className="PopupFeedback" onClick={close}>
+        <div className="PopupFeedback" onClick={close} />
 
-        </div>
         <div className="PopupWindow">
             <div className="Info">
                 <div className="FileForm">Фото</div>
-                <input type="file" className="ButtonAddPhoto"></input>
+                <input type="file" className="ButtonAddPhoto" />
                 <div className="ButtonSend">
                     <img src={logoMail} alt="logoMail"/>
                 </div>
                 <div className="InputBlock">
-                    <input type="text" placeholder="Ваши имя и фамилия" className="InputName"></input>
-                    <textarea type="text" placeholder="Ваш отзыв" className="InputFeedback"></textarea>
+                    <input type="text" placeholder="Ваши имя и фамилия" className="InputName" />
+                    <textarea type="text" placeholder="Ваш отзыв" className="InputFeedback" />
                 </div>
             </div>
         </div>
