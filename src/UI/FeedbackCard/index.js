becaf068022f8ee7ce48aaae39ffc5f5fbf443f3
SIDExSIDE <anoshkinzz@mail.ru>
2020-12-28 02:18:06 +0200

    ADD: Popup

--- src/UI/FeedbackCard/index.js
+++ src/UI/FeedbackCard/index.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import './style.css'
+import {PopupMoreDetails} from "../../components/Feedback/PopupMoreDetails";
 
-export const FeedbackCard = (props) => (
-    <div className="FeedbackCard" style={{marginLeft: props.marginLeft, marginRight: props.marginRight}}>
-        <div className="FeedbackCardTitle">{props.title}</div>
-        <img className="FeedbackCardImage" src={props.img} alt="feedbackImage" />
-        <div className="FeedbackCardContent">"{props.content}"</div>
-        <div className="FeedbackCardButtonMore">Подробнее</div>
-        <div className="FeedbackCardDate">{props.date}</div>
-    </div>
-)
+export const FeedbackCard = (props) => {
+
+    const [popupMoreDetailsToggle, setPopupMoreDetailsToggle] = useState(false)
+
+    const popupMoreDetailsClose = () => {
+        setPopupMoreDetailsToggle(false)
+    }
+
+    return (
+        <div className="FeedbackCard" style={{marginLeft: props.marginLeft, marginRight: props.marginRight}}>
+
+            {
+                (popupMoreDetailsToggle) ? <PopupMoreDetails
+                    title={props.title}
+                    date={props.date}
+                    content={`"` + props.content + `"`}
+                    img={props.img}
+                    close={popupMoreDetailsClose}
+                /> : null
+            }
+
+            <div className="FeedbackCardTitle">{props.title}</div>
+            <img className="FeedbackCardImage" src={props.img} alt="feedbackImage"/>
+            <div className="FeedbackCardContent">
+                {(props.content.length > 90) ?
+                    (`"` + props.content.slice(0, 90) + `..."`) :
+                    (`"` + props.content + `"`)}
+            </div>
+            <div className="FeedbackCardButtonMore" onClick={() => {setPopupMoreDetailsToggle(true)}}>Подробнее</div>
+            <div className="FeedbackCardDate">{props.date}</div>
+        </div>
+    )
+}
--- src/components/Feedback/PopupMoreDetails/index.js
+++ src/components/Feedback/PopupMoreDetails/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import './style.css'
 
-export const PopupMoreDetails = () => (
-    <div className="PopupMoreDetails">
-        <div className="Info">
-            <div className="Photo"></div>
-            <div className="Content">
-                <div className="Title">
-                    <div className="Name">Анна Блабла</div>
-                    <div className="Date">12.12.2000</div>
+export const PopupMoreDetails = (props) => (
+    <>
+        <div className="PopupBackground" onClick={props.close} />
+
+        <div className="PopupMoreDetails">
+            <div className="Info">
+                <img className="Photo" src={props.img} alt="qwe"/>
+                <div className="Content">
+                    <div className="Title">
+                        <div className="Name">{props.title}</div>
+                        <div className="Date">{props.date}</div>
+                    </div>
+                    <div className="Description">{props.content}</div>
                 </div>
-                <div className="Description"></div>
             </div>
         </div>
-    </div>
+    </>
 )
